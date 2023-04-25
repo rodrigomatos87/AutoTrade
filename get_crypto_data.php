dd150f2e88560c79cd5248a7ec4c426ec20c8a38
Rodrigo Matos dos Santos <rodrigo@ravimonitor.com.br>
2023-04-25 16:14:59 -0300

    desenvolvendo

--- get_crypto_data.php
+++ get_crypto_data.php
@@ -13,6 +13,115 @@ function load_data_from_file($filename) {
     return null;
 }
 
+// Históricos ohlcvs
+function download_and_extract_data($symbol, $interval, $candles_count) {
+    // Converte o intervalo para segundos
+    $interval_mapping = [
+        '1m' => 60,
+        '3m' => 180,
+        '5m' => 300,
+        '15m' => 900,
+        '30m' => 1800,
+        '1h' => 3600,
+        '2h' => 7200,
+        '4h' => 14400,
+        '6h' => 21600,
+        '8h' => 28800,
+        '12h' => 43200,
+        '1d' => 86400,
+    ];
+
+    if (!isset($interval_mapping[$interval])) {
+        echo "Intervalo inválido.<br><br>";
+        return;
+    }
+
+    $interval_seconds = $interval_mapping[$interval];
+    $today = date("Y-m-d");
+
+    // Calcule a data de início com base na contagem de candles e no intervalo
+    $start_date = date("Y-m-d", strtotime($today . " -" . ceil($candles_count * $interval_seconds / (24 * 60 * 60)) . " days"));
+
+    $current_date = $start_date;
+
+    $data_folder = 'data';
+
+    // Verifique se a pasta 'data' existe, caso contrário, crie-a
+    if (!file_exists($data_folder)) {
+        mkdir($data_folder, 0777, true);
+    }
+    
+    $all_data = [];
+    while (strtotime($current_date) < strtotime($today)) {
+        $zip_filename = "{$symbol}-{$interval}-{$current_date}.zip";
+        $csv_filename = "{$symbol}-{$interval}-{$current_date}.csv";
+
+        $url = "https://data.binance.vision/data/spot/daily/klines/{$symbol}/{$interval}/{$zip_filename}";
+
+        if (!file_exists("{$data_folder}/{$zip_filename}")) {
+            // Baixe o arquivo .zip se ele não existir
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $data = curl_exec($ch);
+            curl_close($ch);
+
+            file_put_contents("{$data_folder}/{$zip_filename}", $data);
+            echo "Arquivo {$zip_filename} baixado com sucesso.<br><br>";
+        } else {
+            echo "Arquivo {$zip_filename} já existe. Ignorando o download.<br><br>";
+        }
+
+        $zip = new ZipArchive();
+        if ($zip->open("{$data_folder}/{$zip_filename}") === true) {
+            $zip->extractTo($data_folder);
+            $zip->close();
+            echo "Arquivo {$zip_filename} extraído com sucesso.<br><br>";
+
+            if (($handle = fopen("{$data_folder}/{$csv_filename}", "r")) !== false) {
+                while (($row = fgetcsv($handle, 1000, ",")) !== false) {
+                    $all_data[] = $row;
+                }
+                fclose($handle);
+                // Remova o arquivo CSV após ler os dados
+                unlink("{$data_folder}/{$csv_filename}");
+            }
+        } else {
+            echo "Não foi possível extrair o arquivo {$zip_filename}.<br><br>";
+        }
+
+        $current_date = date("Y-m-d", strtotime($current_date . " +1 day"));
+    }
+
+    // Retorne apenas os últimos candles_count dados
+    return array_slice($all_data, -$candles_count);
+}
+
+function get_latest_data($symbol, $interval, $candles_count) {
+    $api_url = "https://api.binance.com/api/v3/klines?symbol={$symbol}&interval={$interval}&limit={$candles_count}";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $api_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $response_data = json_decode($response, true);
+
+    // Converte o formato de resposta da API para o formato CSV usado nos dados históricos
+    $latest_data = [];
+    foreach ($response_data as $entry) {
+        $latest_data[] = [
+            $entry[0], $entry[1], $entry[2], $entry[3], $entry[4], $entry[5]
+        ];
+    }
+
+    return $latest_data;
+}
+
+
+
+/*
 function get_crypto_historical_data($coin_id, $interval, $count) {
     $interval_minutes = intval($interval);
     $total_minutes_needed = $count * $interval_minutes;
@@ -94,7 +203,7 @@ function get_crypto_historical_data($coin_id, $interval, $count) {
 
     return $filtered_data;
 }
-
+*/
 
 
 
--- index.php
+++ index.php
@@ -6,9 +6,74 @@ error_reporting(E_ALL);
 require_once 'get_crypto_data.php';
 require_once 'technical_indicators.php';
 
-$coin_id = 'bitcoin';
-$interval = '15min';
-$count = 500;
+$symbol = "BTCUSDT";
+$interval = "1m";
+$candles_count = 5000;
+
+$current_time = time();
+$current_hour = intval(date('H', $current_time));
+$current_minute = intval(date('i', $current_time));
+
+$interval_mapping = [
+    '1m' => 60,
+    '3m' => 180,
+    '5m' => 300,
+    '15m' => 900,
+    '30m' => 1800,
+    '1h' => 3600,
+    '2h' => 7200,
+    '4h' => 14400,
+    '6h' => 21600,
+    '8h' => 28800,
+    '12h' => 43200,
+    '1d' => 86400,
+];
+
+// Calcule o número total de minutos desde o início do dia
+$total_minutes_today = $current_hour * 60 + $current_minute;
+
+// Calcule o número de candles no dia atual com base no intervalo
+$interval_minutes = $interval_mapping[$interval] / 60;
+$candles_today = floor($total_minutes_today / $interval_minutes);
+
+$historical_candles_count = $candles_count - $candles_today;
+
+$historical_data = download_and_extract_data($symbol, $interval, $historical_candles_count);
+$latest_data = get_latest_data($symbol, $interval, $candles_today);
+
+// Combine os dados históricos e os mais recentes
+$all_data = array_merge($historical_data, $latest_data);
+
+// Retorne apenas os últimos candles_count dados
+$data = array_slice($all_data, -$candles_count);
+
+echo "total: " . count($data) . "<br>";
+
+foreach ($data as $row) {
+    list($timestamp, $open, $high, $low, $close, $volume) = $row;
+    echo "Timestamp: {$timestamp}, Open: {$open}, High: {$high}, Low: {$low}, Close: {$close}, Volume: {$volume}<br>";
+}
+
+
+
+/*
+$historical_data = download_and_extract_data($symbol, $interval, $historical_candles_count);
+$latest_data = get_latest_data($symbol, $interval, $candles_today);
+
+// Combine os dados históricos e os mais recentes
+$all_data = array_merge($historical_data, $latest_data);
+
+// Retorne apenas os últimos candles_count dados
+$data = array_slice($all_data, -$candles_count);
+
+//$data = get_crypto_historical_data($exchange, $pair, $interval, $count);
+
+//$data = download_and_extract_data($symbol, $interval, $candles_count);
+//$data = get_latest_data($symbol, $interval, $candles_count);
+
+echo "<pre>"; print_r($data); echo "</pre>";
+
+exit;
 
 $filename = "historical_data_{$coin_id}_{$interval}.json";
 if (file_exists($filename)) {
@@ -21,6 +86,9 @@ if (file_exists($filename)) {
     // Salva os dados históricos em um arquivo
     file_put_contents($filename, json_encode($data));
 }
+*/
+
+
 
 function save_opportunity_to_file($opportunity, $filename) {
     $file = fopen($filename, 'a');
@@ -28,11 +96,9 @@ function save_opportunity_to_file($opportunity, $filename) {
     fclose($file);
 }
 
-echo "prices: " . count($data['prices']) . "<br>";
-echo "market_caps: " . count($data['market_caps']) . "<br>";
-echo "total_volumes: " . count($data['total_volumes']) . "<br>";
-
-exit;
+// echo "prices: " . count($data['prices']) . "<br>";
+// echo "market_caps: " . count($data['market_caps']) . "<br>";
+// echo "total_volumes: " . count($data['total_volumes']) . "<br>";
 
 // Calcule os indicadores
 $ema50 = ema($data, 50);
@@ -70,5 +136,3 @@ for ($i = max(50, 200, 14, 20); $i < count($data['prices']); $i++) {
 }
 
 ?>
-
-
